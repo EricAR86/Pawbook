c3afe8de724569c6ae1ba86361994dc76b1d4ff0
Emmanuel Alejandro Borrayo Cortes <alexborrayo21@hotmail.com>
2022-05-01 13:35:25 -0500

    Se elimina ruta

--- app.js
+++ app.js
@@ -30,8 +30,6 @@ app.use("/", index);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
-const posts = require("./routes/posts.routes")
-app.use("/", posts)
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
